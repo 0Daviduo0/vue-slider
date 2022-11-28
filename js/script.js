553096a434ab441f1e16afd92839308fb5c1ac67
Daviduo <davide0400@gmail.com>
2022-11-28 17:57:43 +0100

    adding thumbnails and class for active state

--- js/script.js
+++ js/script.js
@@ -24,10 +24,12 @@
 
 // console.log(slides);
 
+const { createApp } = Vue;
+
 createApp({
     data() {
         return {
-            shownImg: 0,
+            indexElem: 0,
             film: {
                 imgs: [
                     'img/01.webp',
@@ -35,10 +37,22 @@ createApp({
                     'img/03.webp',
                     'img/04.webp',
                     'img/05.webp',
+                ],
+                title: [
+                    'Marvel\'s Spiderman Miles Morales',
+                    'Ratchet & Clank: Rift Apart',
+                    'Fortnite',
+                    'Stray',
+                    'Marvel\'s Avengers',
+                ],
+                text: [
+                    'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                    'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
+                    'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                 ]
             }
-
-
         }
     }
-})
+}).mount('#app')
